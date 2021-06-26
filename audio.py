4ff52954b05b50a8cc6c3a7f1b18e218841ea482
Koen Habets <koenhabets1@gmail.com>
2021-06-26 23:28:57 +0200

    Publish mqtt message when computer is idle for specified time

--- audio.py
+++ audio.py
@@ -1,4 +1,21 @@
 import sys
+import json
+#from datetime import datetime
+
+from ctypes import Structure, windll, c_uint, sizeof, byref
+
+class LASTINPUTINFO(Structure):
+    _fields_ = [
+        ('cbSize', c_uint),
+        ('dwTime', c_uint),
+    ]
+
+def get_idle_duration():
+    lastInputInfo = LASTINPUTINFO()
+    lastInputInfo.cbSize = sizeof(lastInputInfo)
+    windll.user32.GetLastInputInfo(byref(lastInputInfo))
+    millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
+    return millis / 1000.0
 
 if not sys.platform.startswith("linux"):
     from pycaw.pycaw import AudioUtilities
@@ -11,7 +28,11 @@ import time
 
 ignoreList = ["Battle.net.exe", "AudioRelay.exe", "Steam.exe", "RAVCpl64.exe", "ShellExperienceHost.exe",
               "cef_browser_process.exe",
-              "taskhostw.exe", "AMDRSServ.exe", "RadeonSoftware.exe", "explorer.exe", "Telegram.exe", "PeopleApp.exe"]
+              "taskhostw.exe", "AMDRSServ.exe", "RadeonSoftware.exe", "explorer.exe", "Telegram.exe",
+              "PeopleApp.exe", "steamwebhelper.exe", "obs64.exe", "steam.exe"]
+lastPublishedState = False;
+idleTimeLimit = 120
+count = 0
 
 client = mqtt.Client()
 
@@ -47,8 +68,21 @@ def on_connect(client, userdata, flags, rc):
 
 def on_disconnect(client, userdate, rc):
     print("Disconnected")
-    client.reconnect()
+    disconnected = True
+    while disconnected:
+        try:
+            client.reconnect()
+            disconnected = False
+            publish_last_state()
+        except:
+            print("Reconnect failed")
+            time.sleep(5)
 
+def publish_last_state():
+    if lastPublishedState:
+        client.publish("pc/active", "ON")
+    else:
+        client.publish("pc/active", "OFF")
 
 def on_message(client, userdata, msg):
     if sys.platform.startswith("linux"):
@@ -65,24 +99,26 @@ def on_message(client, userdata, msg):
             setVolumeProcess(sessions[2].proplist["application.name"], float(message))
     else:
         sessions = getSessionsWindows()
-        # print(msg.topic + " " + str(msg.payload))
-        message = str(msg.payload)[3:]
-        message = message[:-1]
-        print(message)
-        if msg.topic == "pc/audio/set/1":
-            setVolumeProcess(sessions[0].Process.name(), float(message))
-        elif msg.topic == "pc/audio/set/2":
-            setVolumeProcess(sessions[1].Process.name(), float(message))
-        elif msg.topic == "pc/audio/set/3":
-            setVolumeProcess(sessions[2].Process.name(), float(message))
+        if msg.topic[0:2] == "pc":
+            try:
+                message = str(msg.payload)[3:]
+                message = message[:-1]
+                if msg.topic == "pc/audio/set/1":
+                    setVolumeProcess(sessions[0].Process.name(), float(message))
+                elif msg.topic == "pc/audio/set/2":
+                    setVolumeProcess(sessions[1].Process.name(), float(message))
+                elif msg.topic == "pc/audio/set/3":
+                    setVolumeProcess(sessions[2].Process.name(), float(message))
+            except:
+                print("Not found")
 
 
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_disconnect = on_disconnect
-client.username_pw_set("woonkamer-pc", password="ijflmtSktpftqwn6R7UI")
-client.will_set("pc/audio/display", "", qos=0, retain=False)
-client.connect("mqtt.koenhabets.nl", 1883, 60)
+client.username_pw_set("username", password="")
+client.will_set("topic", "OFF", qos=0, retain=False)
+client.connect_async("", 1883, 60)
 
 
 def setVolumeProcess(process, vol):
@@ -101,34 +137,34 @@ def setVolumeProcess(process, vol):
 
 
 def publishProcess():
-    while True:
-        processList = []
-        if not sys.platform.startswith("linux"):
-            sessions = getSessionsWindows()
-            for session in sessions:
-                if session.Process:
-                    processList.append(session.Process.name())
-        else:
-            sessions = getSessionsLinux()
-            for session in sessions:
-                processList.append(session.proplist["application.name"])
+    processList = []
+    if not sys.platform.startswith("linux"):
+        sessions = getSessionsWindows()
+        for session in sessions:
+            if session.Process:
+                processList.append(session.Process.name())
+    else:
+        sessions = getSessionsLinux()
+        for session in sessions:
+            processList.append(session.proplist["application.name"])
 
-        publishString = ""
-        processCount = len(processList)
-        if processCount >= 3:
-            processCount = 3
+    publishString = ""
+    processCount = len(processList)
+    if processCount >= 3:
+        processCount = 3
 
-        for i in range(0, processCount):
-            processName = processList[i][0:3]
-            publishString = publishString + processName + "/"
+    for i in range(0, processCount):
+        processName = processList[i][0:3]
+        publishString = publishString + processName + "/"
 
-        publishString = publishString[:-1]
-        client.publish("pc/audio/display", publishString)
-        time.sleep(10)
+
+    publishString = publishString[:-1]
+    client.publish("pc/audio/display", publishString)
 
 
 if __name__ == "__main__":
     client.loop_start()
+    time.sleep(2)
     if not sys.platform.startswith("linux"):
         sessions = getSessionsWindows()
         for session in sessions:
@@ -140,5 +176,24 @@ if __name__ == "__main__":
         for session in sessions:
             print(session.proplist["application.name"])
 
-    publishProcess()
+    while True:
+        publishProcess()
+        print(get_idle_duration())
+        if (get_idle_duration() > idleTimeLimit):
+            if lastPublishedState:
+                client.publish("pc/active", "OFF")
+                lastPublishedState = False
+        else:
+            if not lastPublishedState:
+                client.publish("pc/active", "ON")
+                lastPublishedState = True
+            
+        count = count + 1
+        if count > 6:
+            #client.publish("pc/time", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+            publish_last_state()
+
+            count = 0
+        
+        time.sleep(8)
     # client.loop_forever(timeout=1.0, max_packets=1, retry_first_connection=True)
